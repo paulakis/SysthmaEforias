37dac635081bed89c26ed3aeecbb1f8975321544
PanosPavlou <polykarposepi@hotmail.gr>
2017-11-11 09:42:31 +0200

    allages se auto pou aposteilame

---
+++ SysthmaEforias/src/testers/SecondTester.java
@@ -0,0 +1,14 @@
+package testers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SecondTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
